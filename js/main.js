8292ac4137f25c21ed8b0b40e68ec3d0d46c2dec
Mustafa <mustafa.ramazanov.2002@gmail.com>
2023-05-25 20:19:50 +0300

    adaptive

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper(".swiper", {
+    effect: "fade",
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+      delay: 2700,
+      disableOnInteraction:false,
+    },
+  });
+
+  function onEntry(entry){
+    entry.forEach(change => {
+      if (change.isIntersecting){
+        change.target.classList.add('element-show')
+      }
+    })
+  }
+
+  let options = {
+    threshold:[0.5] };
+  let observer = new
+  IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('header')
+
+  for(let elm of elements){
+    observer.observe(elm)
+  }
